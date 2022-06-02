98d32eb6e57d3b3e33394c241c3c0c7c6c8b360a
Kenan Dropić <dropic5@hotmail.com>
2022-06-02 19:07:38 +0200

    sign up page

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as LandingPageP } from "./LandingPage";
+export { default as SignInP } from "./SignIn";
+export { default as SignUpP } from "./SignUp";
+export { default as ProfileP } from "./Profile";
