b4eecc892c4aaee23b0aa6cf25fa6308ccca852a
Kenan Dropić <dropic5@hotmail.com>
2022-06-03 12:03:28 +0200

    error responses

---
+++ backend/utils/errorResponse.js
@@ -0,0 +1,26 @@
+export class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 400;
+  }
+}
+
+export class UnAuthenticatedError extends BadRequestError {
+  constructor(message) {
+    super(message);
+    this.statusCode = 401;
+  }
+}
+
+export class NotFoundError extends BadRequestError {
+  constructor(message) {
+    super(message);
+    this.statusCode = 404;
+  }
+}
+export class UnAuthorizedError extends NotFoundError {
+  constructor(message) {
+    super(message);
+    this.statusCode = 403;
+  }
+}
