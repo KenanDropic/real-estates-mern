bc1c0a3bedec51b87d97968c934429d20670737f
Kenan Dropić <dropic5@hotmail.com>
2022-11-18 08:55:08 +0100

    fix uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI, {
+    const connection = await mongoose.connect(`${process.env.MONGO_URI}`, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
